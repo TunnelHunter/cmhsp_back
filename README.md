20d4f1ab1e5b229cfaacfb607df9e0cd447233d3
yangbo <1071011167@qq.com>
2018-04-12 11:53:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# cmhsp_back
---
+++ src/main/java/com/cmhsp/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.cmhsp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+}
