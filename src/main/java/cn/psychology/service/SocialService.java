24af7b4b8244fe0e9ebab8da82275d631c9b2717
XYeChen <x1470123@163.com>
2018-06-13 10:31:32 +0800

    social部分修改
    userid 变为userId

--- src/main/java/cn/psychology/service/SocialService.java
+++ src/main/java/cn/psychology/service/SocialService.java
@@ -3,9 +3,8 @@ package cn.psychology.service;
 import cn.psychology.entity.Social;
 
 public interface SocialService {
-    public void addsocialtofavorite(Integer uid,String uname,String sid);
+    public void addsocialtofavorite(Integer uid,String uname,Integer sid);
     public void  addsocial(Integer uid,String img,String text,String addtime);
-    public void  addcomment(String cuid,String sid,String cd,String ct);
-
+    public void  addcomment(Integer cuid,Integer sid,String cd,String ct);
 
 }
