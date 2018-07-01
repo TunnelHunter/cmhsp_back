86483ebfc5f65a1e2432c016088b4b95db40e945
MountainC44 <1071011167@qq.com>
2018-07-01 11:00:13 +0800

    修改数据库文件

--- db_file/mysql/cmhsp_db.sql
+++ db_file/mysql/cmhsp_db.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : mysql
+ Source Server         : bobness
  Source Server Type    : MySQL
- Source Server Version : 50720
+ Source Server Version : 50719
  Source Host           : localhost
- Source Database       : xuxuxu
+ Source Database       : cmhsp_db
 
  Target Server Type    : MySQL
- Target Server Version : 50720
+ Target Server Version : 50719
  File Encoding         : utf-8
 
- Date: 06/25/2018 16:03:16 PM
+ Date: 07/01/2018 10:58:40 AM
 */
 
 SET NAMES utf8;
@@ -33,7 +33,7 @@ CREATE TABLE `comb_usersysnews` (
 --  Records of `comb_usersysnews`
 -- ----------------------------
 BEGIN;
-INSERT INTO `comb_usersysnews` VALUES ('1', '0', '1', '1'), ('2', '0', '1', '2'), ('3', '0', '1', '3'), ('4', '0', '1', '4'), ('5', '0', '2', '1'), ('6', '0', '2', '2'), ('7', '0', '2', '3'), ('8', '0', '2', '4'), ('9', '0', '3', '1'), ('10', '0', '3', '2'), ('11', '0', '3', '3'), ('12', '0', '3', '4'), ('13', '0', '4', '1'), ('14', '0', '4', '2'), ('15', '0', '4', '3'), ('16', '0', '4', '4'), ('17', '0', '5', '1'), ('18', '0', '5', '2'), ('19', '0', '5', '3'), ('20', '0', '5', '4');
+INSERT INTO `comb_usersysnews` VALUES ('1', '0', '1', '1'), ('2', '0', '1', '2'), ('3', '0', '1', '3'), ('4', '1', '1', '4'), ('5', '0', '2', '1'), ('6', '0', '2', '2'), ('7', '0', '2', '3'), ('8', '1', '2', '4'), ('9', '0', '3', '1'), ('10', '0', '3', '2'), ('11', '0', '3', '3'), ('12', '1', '3', '4'), ('13', '0', '4', '1'), ('14', '0', '4', '2'), ('15', '0', '4', '3'), ('16', '1', '4', '4'), ('17', '0', '5', '1'), ('18', '0', '5', '2'), ('19', '0', '5', '3'), ('20', '1', '5', '4');
 COMMIT;
 
 -- ----------------------------
@@ -86,7 +86,7 @@ CREATE TABLE `systemnews` (
 --  Records of `systemnews`
 -- ----------------------------
 BEGIN;
-INSERT INTO `systemnews` VALUES ('1', '这是第一条系统消息。', 'Thu Jun 21 20:25:28 CST 2018'), ('2', '这是第二条系统消息。', 'Thu Jun 21 20:25:34 CST 2018'), ('3', '这是第三条系统消息。', 'Thu Jun 21 20:25:38 CST 2018'), ('4', '这是第四条系统消息。', 'Thu Jun 21 20:25:42 CST 2018'), ('5', '这是第五条系统消息。', 'Thu Jun 21 20:25:49 CST 2018');
+INSERT INTO `systemnews` VALUES ('1', 'app版本更新', '2018-07-01 09:00'), ('2', '您的测试分析出炉', '2018-07-01 09:00'), ('3', '增加抑郁症测试题', '2018-07-01 09:00'), ('4', '亲，每天要早睡早起哦', '2018-06-11 09:00'), ('5', '社区评论功能开放', '2018-06-01 09:00');
 COMMIT;
 
 -- ----------------------------
@@ -99,17 +99,17 @@ CREATE TABLE `testresult` (
   `examination_id` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `examination_score` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `summary` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `test_time` bigint(20) NOT NULL,
+  `test_time` varchar(255) COLLATE utf8_bin NOT NULL,
   `test_type` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `user_id` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`score_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 --  Records of `testresult`
 -- ----------------------------
 BEGIN;
-INSERT INTO `testresult` VALUES ('1', '3', '1', '80', null, '12222222', '1', '3'), ('2', '3', '2', '50', null, '12222222', '2', '3'), ('3', '3', '3', '70', null, '12222222', '2', '3'), ('4', '3', '3', '75', null, '12222222', '2', '3'), ('5', '3', '1', '85', null, '12222222', '1', '3'), ('6', '3', '1', '85', null, '12222222', '1', '4'), ('7', '3', '3', '75', null, '12222222', '2', '4'), ('8', '3', '3', '75', null, '12222222', '2', '1'), ('9', '3', '1', '75', null, '12222222', '1', '1');
+INSERT INTO `testresult` VALUES ('1', '3', '1', '80', null, '2018-06-01 19:29', '1', '3'), ('2', '3', '2', '50', null, '2018-06-02 19:29', '2', '3'), ('3', '3', '3', '70', null, '2018-06-03 19:29', '2', '3'), ('4', '3', '3', '75', null, '2018-06-04 19:29', '2', '3'), ('5', '3', '1', '85', null, '2018-06-05 19:29', '1', '3'), ('6', '3', '1', '85', null, '2018-06-06 19:29', '1', '4'), ('7', '3', '3', '75', null, '2018-06-07 19:29', '2', '4'), ('8', '3', '3', '75', null, '2018-06-08 19:29', '2', '1'), ('9', '3', '1', '75', null, '2018-06-11 19:29', '1', '1'), ('10', '5', '1', '46', '有较严重抑郁', '2018-06-12 19:29', null, '4'), ('11', '3', '3', '63', '你的焦虑有点发烧。', '2018-06-15 19:29', null, '4'), ('12', '1', '1', '5', '没有抑郁症', '2018-06-16 19:29', null, '4'), ('13', '6', '1', '57', '有严重抑郁', '2018-06-21 19:29', null, '4'), ('14', '1', '1', '0', '没有抑郁症', '2018-06-22 19:29', null, '3'), ('15', '2', '2', '47', '轻微焦虑', '2018-06-26 12:29', null, '2'), ('16', '6', '1', '63', '有严重抑郁', '2018-06-29 19:26', null, '4'), ('17', '1', '1', '2', '没有抑郁症', '2018-06-30 19:29', null, '4');
 COMMIT;
 
 -- ----------------------------
@@ -127,13 +127,13 @@ CREATE TABLE `user` (
   `usernickname` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `userpwd` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 --  Records of `user`
 -- ----------------------------
 BEGIN;
-INSERT INTO `user` VALUES ('1', '', '', '', '', '', 'liuchangwei', '', '123'), ('2', 'dd', 'sssqsss', '男', 'sas', '', 'haomiao', '大梦迷离藏小剑', '1234'), ('3', '', '', '', '', '', 'xuyechen', '', '123'), ('4', '', '', '', '', '', 'xueyangbo', '', '123'), ('5', '', '', '', '', '', 'ceshi1', '', '123'), ('6', '', '', '', '', '', 'ceshi2', '', '123');
+INSERT INTO `user` VALUES ('1', 'imgs/touxiangman.png', '', '', '', '', 'liuchangwei', '', '123'), ('2', 'imgs/touxiangman.png', 'sssqsss', '男', 'sas', '', 'haomiao', '大梦迷离藏小剑', '1234'), ('3', 'imgs/touxiangman.png', '', '', '', '', 'xuyechen', '', '123'), ('4', 'imgs/touxiangman.png', '山西运城市', '男', '从心开始', '', 'xueyangbo', '', '123'), ('5', 'imgs/default_userImage.png', '', '', '', '', 'ceshi1', '', '123'), ('6', 'imgs/default_userImage.png', '', '', '', '', 'ceshi2', '', '123'), ('7', 'imgs/default_userImage.png', '', '', '从心开始', '', 'hehe', 'hehe', '123'), ('8', 'imgs/default_userImage.png', '', '', '从心开始', '', 'hehehehe', 'hehehehe', '123');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
